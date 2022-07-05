b7e2d763edc19cce5489834db181662df818642e
Henri Azevedo <hnr.azevedo@gmail.com>
2022-07-05 14:00:12 -0300

    remove directores files

--- opt/postfixadmin/config.hazevedo.inc.php
+++
@@ -1,5 +0,0 @@
-$CONF['database_type'] = 'mysqli';
-$CONF['database_host'] = 'localhost';
-$CONF['database_user'] = 'postfixadmin';
-$CONF['database_password'] = 'password';
-$CONF['database_name'] = 'postfixadmin';
--- opt/postfixadmin/create_database.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE postfixadmin;
-CREATE USER 'postfixadmin'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON postfixadmin.* TO 'postfixadmin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
